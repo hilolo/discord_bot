3ebdda4d4f2a3e0c2ff7709697174bfe6c64a3a1
hilolo <37957264+hilolo@users.noreply.github.com>
2020-04-04 04:42:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,30 @@
 const Discord = require("discord.js");
 const myid = ['Your ID']
 const client = new Discord.Client();
-
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
 
 client.login(process.env.TOKEN);
+client2.login(process.env.TOKEN2);
+client3.login(process.env.TOKEN3);
+client4.login(process.env.TOKEN4);
+client5.login(process.env.TOKEN5);
+client6.login(process.env.TOKEN6);
+client7.login(process.env.TOKEN7); 
+client8.login(process.env.TOKEN8); 
+client9.login(process.env.TOKEN9); 
+client10.login(process.env.TOKEN10);
 
 
 
-
-client.on('message', message => {
+ client.on('message', message => {
   if (message.content === '$d') {
 message.channel.send('#daily')
   }
@@ -20,16 +36,19 @@ message.channel.send('#credits')
      message.channel.send(text);
   }
 
-  if (message.content === '$spam') {
-    let count = 0;
-    let ecount = 0;
-    for(let x = 0; x < 9000; x++) {
-      message.channel.send(`**hilolo Zwiiiin **[ " ${x} " ]`)
-        .then(m => {
-          count++;
-        })
-        
+  if (message.content === '$hilala') {
+
+    const sleep = require('util').promisify(setTimeout);
+    (async function main() {
+      let i = 10;
+      while (i--) {
+        message.channel.send(makeid(5));
+        if(i % 2 != 0 )  message.channel.send(makeid(5) + "  " + makeid(i));
+        await sleep(8000);
       }
+    }());
+
+      
     }
     if(message.content === '!rep'){
       message.channel.send("#rep "+"<@" + myid + ">")
@@ -37,5 +56,331 @@ message.channel.send('#credits')
 
 });
 
+
+
+client2.on('message', message => {
+  if (message.content === '$d') {
+message.channel.send('#daily')
+  }
+  if(message.content === '$c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("$s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+
+  if (message.content === '$hilala') {
+
+    const sleep = require('util').promisify(setTimeout);
+    (async function main() {
+      let i = 10;
+      while (i--) {
+        message.channel.send(makeid(5));
+        if(i % 2 != 0 )  message.channel.send(makeid(5) + "  " + makeid(i));
+        await sleep(8000);
+      }
+    }());
+
+      
+    }
+    if(message.content === '!rep'){
+      message.channel.send("#rep "+"<@" + myid + ">")
+  }
+
+});
+
+
+
+
+client3.on('message', message => {
+  if (message.content === '$d') {
+message.channel.send('#daily')
+  }
+  if(message.content === '$c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("$s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+
+  if (message.content === '$hilala') {
+
+    const sleep = require('util').promisify(setTimeout);
+    (async function main() {
+      let i = 10;
+      while (i--) {
+        message.channel.send(makeid(5));
+        if(i % 2 != 0 )  message.channel.send(makeid(5) + "  " + makeid(i));
+        await sleep(8000);
+      }
+    }());
+
+      
+    }
+    if(message.content === '!rep'){
+      message.channel.send("#rep "+"<@" + myid + ">")
+  }
+
+});
+
+
+
+
+
+client4.on('message', message => {
+  if (message.content === '$d') {
+message.channel.send('#daily')
+  }
+  if(message.content === '$c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("$s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+
+  if (message.content === '$hilala') {
+
+    const sleep = require('util').promisify(setTimeout);
+    (async function main() {
+      let i = 10;
+      while (i--) {
+        message.channel.send(makeid(5));
+        if(i % 2 != 0 )  message.channel.send(makeid(5) + "  " + makeid(i));
+        await sleep(8000);
+      }
+    }());
+
+      
+    }
+    if(message.content === '!rep'){
+      message.channel.send("#rep "+"<@" + myid + ">")
+  }
+
+});
+
+
+
+
+client5.on('message', message => {
+  if (message.content === '$d') {
+message.channel.send('#daily')
+  }
+  if(message.content === '$c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("$s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+
+  if (message.content === '$hilala') {
+
+    const sleep = require('util').promisify(setTimeout);
+    (async function main() {
+      let i = 10;
+      while (i--) {
+        message.channel.send(makeid(5));
+        if(i % 2 != 0 )  message.channel.send(makeid(5) + "  " + makeid(i));
+        await sleep(8000);
+      }
+    }());
+
+      
+    }
+    if(message.content === '!rep'){
+      message.channel.send("#rep "+"<@" + myid + ">")
+  }
+
+});
+
+
+
+client6.on('message', message => {
+  if (message.content === '$d') {
+message.channel.send('#daily')
+  }
+  if(message.content === '$c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("$s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+
+  if (message.content === '$hilala') {
+
+    const sleep = require('util').promisify(setTimeout);
+    (async function main() {
+      let i = 10;
+      while (i--) {
+        message.channel.send(makeid(5));
+        if(i % 2 != 0 )  message.channel.send(makeid(5) + "  " + makeid(i));
+        await sleep(8000);
+      }
+    }());
+
+      
+    }
+    if(message.content === '!rep'){
+      message.channel.send("#rep "+"<@" + myid + ">")
+  }
+
+});
+
+
+
+
+
+client7.on('message', message => {
+  if (message.content === '$d') {
+message.channel.send('#daily')
+  }
+  if(message.content === '$c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("$s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+
+  if (message.content === '$hilala') {
+
+    const sleep = require('util').promisify(setTimeout);
+    (async function main() {
+      let i = 10;
+      while (i--) {
+        message.channel.send(makeid(5));
+        if(i % 2 != 0 )  message.channel.send(makeid(5) + "  " + makeid(i));
+        await sleep(8000);
+      }
+    }());
+
+      
+    }
+    if(message.content === '!rep'){
+      message.channel.send("#rep "+"<@" + myid + ">")
+  }
+
+});
+
+
+
+client8.on('message', message => {
+  if (message.content === '$d') {
+message.channel.send('#daily')
+  }
+  if(message.content === '$c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("$s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+
+  if (message.content === '$hilala') {
+
+    const sleep = require('util').promisify(setTimeout);
+    (async function main() {
+      let i = 10;
+      while (i--) {
+        message.channel.send(makeid(5));
+        if(i % 2 != 0 )  message.channel.send(makeid(5) + "  " + makeid(i));
+        await sleep(8000);
+      }
+    }());
+
+      
+    }
+    if(message.content === '!rep'){
+      message.channel.send("#rep "+"<@" + myid + ">")
+  }
+
+});
+
+
+
+
+client9.on('message', message => {
+  if (message.content === '$d') {
+message.channel.send('#daily')
+  }
+  if(message.content === '$c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("$s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+
+  if (message.content === '$hilala') {
+
+    const sleep = require('util').promisify(setTimeout);
+    (async function main() {
+      let i = 10;
+      while (i--) {
+        message.channel.send(makeid(5));
+        if(i % 2 != 0 )  message.channel.send(makeid(5) + "  " + makeid(i));
+        await sleep(8000);
+      }
+    }());
+
+      
+    }
+    if(message.content === '!rep'){
+      message.channel.send("#rep "+"<@" + myid + ">")
+  }
+
+});
+
+
+
+
+
+client10.on('message', message => {
+  if (message.content === '$d') {
+message.channel.send('#daily')
+  }
+  if(message.content === '$c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("$s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+
+  if (message.content === '$hilala') {
+
+    const sleep = require('util').promisify(setTimeout);
+    (async function main() {
+      let i = 10;
+      while (i--) {
+        message.channel.send(makeid(5));
+        if(i % 2 != 0 )  message.channel.send(makeid(5) + "  " + makeid(i));
+        await sleep(8000);
+      }
+    }());
+
+      
+    }
+    if(message.content === '!rep'){
+      message.channel.send("#rep "+"<@" + myid + ">")
+  }
+
+});
+
+
+
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 
